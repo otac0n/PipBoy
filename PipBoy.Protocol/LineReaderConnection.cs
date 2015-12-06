850898ac68d2daeafd9e2c03795f8bf2c77cafd8
John Gietzen <otac0n@gietzen.us>
2015-12-06 12:30:18 -0800

    Added simple connection classes.

---
+++ PipBoy.Protocol/LineReaderConnection.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace PipBoy.Protocol
+{
+    public class LineReaderConnection : IDisposable
+    {
+        private const int MaxSize = 1024;
+
+        private readonly TcpConnection tcpConnection;
+
+        public LineReaderConnection()
+        {
+            this.tcpConnection = new TcpConnection();
+        }
+
+        public async Task ConnectAsync(EndPoint endPoint)
+        {
+            await this.tcpConnection.ConnectAsync(endPoint);
+        }
+
+        public async Task<Tuple<byte, byte[]>> ReceiveAsync()
+        {
+            var header = new byte[sizeof(int) + 1];
+
+            var read = 0;
+            while (read < header.Length)
+            {
+                var count = await this.tcpConnection.ReceiveAsync(header, read, header.Length - read);
+                if (count == 0)
+                {
+                    return null;
+                }
+
+                read += count;
+            }
+
+            var payloadSize = BitConverter.ToInt32(header, 0);
+            var type = header[sizeof(int)];
+            var payload = new byte[payloadSize];
+
+            read = 0;
+            while (read < payloadSize)
+            {
+                var count = await this.tcpConnection.ReceiveAsync(payload, read, Math.Min(payloadSize - read, MaxSize));
+                if (count == 0)
+                {
+                    return null;
+                }
+
+                read += count;
+            }
+
+            return Tuple.Create(type, payload);
+        }
+
+        public async Task SendAsync(byte type, byte[] data)
+        {
+            // Header format:
+            // 0x00000000 data length
+            // 0x00       type
+            // 0x...      data
+            var header = BitConverter.GetBytes(data.Length);
+            Array.Resize(ref header, sizeof(int) + 1);
+            header[sizeof(int)] = type;
+
+            await this.tcpConnection.SendAsync(header, 0, header.Length);
+            for (var offset = 0; offset < data.Length; offset += MaxSize)
+            {
+                await this.tcpConnection.SendAsync(data, offset, Math.Min(data.Length - offset, MaxSize));
+            }
+        }
+
+        public void Dispose()
+        {
+            this.tcpConnection.Dispose();
+        }
+    }
+}
---
+++ PipBoy.Protocol/TcpConnection.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace PipBoy.Protocol
+{
+    public class TcpConnection : IDisposable
+    {
+        private readonly Socket socket;
+
+        public TcpConnection()
+        {
+            this.socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public Task ConnectAsync(EndPoint endPoint)
+        {
+            var result = new TaskCompletionSource<byte>();
+
+            var connectInfo = new SocketAsyncEventArgs
+            {
+                RemoteEndPoint = endPoint,
+            };
+
+            EventHandler<SocketAsyncEventArgs> complete = (s, a) =>
+            {
+                if (a.SocketError != SocketError.Success)
+                {
+                    result.SetException(new SocketException((int)a.SocketError));
+                }
+                else
+                {
+                    result.SetResult(0);
+                }
+            };
+            connectInfo.Completed += complete;
+
+            try
+            {
+                if (!this.socket.ConnectAsync(connectInfo))
+                {
+                    complete(this.socket, connectInfo);
+                }
+            }
+            catch (Exception ex)
+            {
+                result.SetException(ex);
+            }
+
+            return result.Task;
+        }
+
+        public Task<int> ReceiveAsync(byte[] buffer, int offset, int count)
+        {
+            var result = new TaskCompletionSource<int>();
+
+            var connectInfo = new SocketAsyncEventArgs
+            {
+                BufferList = new[] { new ArraySegment<byte>(buffer) },
+            };
+
+            EventHandler<SocketAsyncEventArgs> complete = (s, a) =>
+            {
+                if (a.SocketError != SocketError.Success)
+                {
+                    result.SetException(new SocketException((int)a.SocketError));
+                }
+                else
+                {
+                    result.SetResult(a.BytesTransferred);
+                }
+            };
+            connectInfo.Completed += complete;
+
+            try
+            {
+                if (!this.socket.ReceiveAsync(connectInfo))
+                {
+                    complete(this.socket, connectInfo);
+                }
+            }
+            catch (Exception ex)
+            {
+                result.SetException(ex);
+            }
+
+            return result.Task;
+        }
+
+        public Task SendAsync(byte[] buffer, int offset, int count)
+        {
+            var result = new TaskCompletionSource<int>();
+
+            var connectInfo = new SocketAsyncEventArgs
+            {
+                BufferList = new[] { new ArraySegment<byte>(buffer, offset, count) },
+            };
+
+            EventHandler<SocketAsyncEventArgs> complete = (s, a) =>
+            {
+                if (a.SocketError != SocketError.Success)
+                {
+                    result.SetException(new SocketException((int)a.SocketError));
+                }
+                else
+                {
+                    result.SetResult(a.BytesTransferred);
+                }
+            };
+            connectInfo.Completed += complete;
+
+            try
+            {
+                if (!this.socket.SendAsync(connectInfo))
+                {
+                    complete(this.socket, connectInfo);
+                }
+            }
+            catch (Exception ex)
+            {
+                result.SetException(ex);
+            }
+
+            return result.Task;
+        }
+
+        public void Dispose()
+        {
+            this.socket.Dispose();
+        }
+    }
+}
